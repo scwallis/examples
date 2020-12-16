d03b3f5d949e59316d137ca59462abf65c28ceda
wallis <scwallis@gmail.com>
2020-12-16 12:32:20 +0900

    Update k8s_setup.sh

--- staging/storage/minio/k8s_setup.sh
+++ staging/storage/minio/k8s_setup.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-sudo apt-get install -y apt-transport-https curl
+sudo apt-get update
+sudo apt-get upgrade
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 
-# 자동업데이트 방지
+# 패키지가 자동으로 설치, 업그레이드, 제거되지 않도록 hold함.
 sudo apt-mark hold kubelet kubeadm kubectl
 
+# 설치 완료 확인
 kubeadm version
 kubelet --version
 kubectl version
